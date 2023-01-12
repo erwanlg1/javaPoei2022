9dcde5051fdc0e3361d40d9e274404a6a6ab1def
Erwan Le Guillou <erwan.leguillou@yahoo.fr>
2023-01-12 13:46:43 +0100

    exo1/2/3

---
+++ com/centraleNantes/poei2/ErwanExercice/JourSemaineSwitch/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int day;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Un chiffre entre 1 et 7");
+
+        day = keyboard.nextInt();
+
+        switch (day) {
+            case 1 -> System.out.println("Lundi");
+            case 2 -> System.out.println("Mardi");
+            case 3 -> System.out.println("Mercredi");
+            case 4 -> System.out.println("Jeudi");
+            case 5 -> System.out.println("Vendredi");
+            case 6 -> System.out.println("Samedi");
+            case 7 -> System.out.println("Dimanche");
+            default -> System.out.println("Ne correspond à aucun jours");
+        }
+
+
+        switch (day) {
+            case 1:
+                System.out.println("Lundi");
+                break;
+
+            case 2:
+                System.out.println("Mardi");
+                break;
+
+            case 3:
+                System.out.println("Mercredi");
+                break;
+
+            case 4:
+                System.out.println("Jeudi");
+                break;
+
+            case 5:
+                System.out.println("Vendredi");
+                break;
+
+            case 6:
+                System.out.println("Samedi");
+                break;
+
+            case 7:
+                System.out.println("Dimanche");
+                break;
+
+            default:
+                System.out.println("Ne correspond à aucun jours");
+        }
+    }
+}
+
+
+
---
+++ com/centraleNantes/poei2/ErwanExercice/JoursSemaineIf/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int day;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Un chiffre entre 1 et 7");
+
+        day = keyboard.nextInt();
+
+        if (day == 1) {
+            System.out.println("Le jour de la semaine est : Lundi");
+        }
+
+        else if (day == 2) {
+            System.out.println("Le jour de la semaine est : Mardi");
+        }
+        else if (day == 3) {
+            System.out.println("Le jour de la semaine est : Mercredi");
+        }
+        else if (day == 4) {
+            System.out.println("Le jour de la semaine est : Jeudi");
+        }
+        else if (day == 5) {
+            System.out.println("Le jour de la semaine est : Vendredi");
+        }
+        else if (day == 6) {
+            System.out.println("Le jour de la semaine est : Samedi");
+        }
+        else if (day == 7) {
+            System.out.println("Le jour de la semaine est : Dimanche");
+        }
+
+        else {
+            System.out.println("Ce n'est pas un jours de la semaine");
+        }
+
+    }
+}
