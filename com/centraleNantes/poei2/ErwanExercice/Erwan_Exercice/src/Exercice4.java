522948d030cabe050faadab20c41c561129523f8
Erwan Le Guillou <erwan.leguillou@yahoo.fr>
2023-01-12 14:28:21 +0100

    exercice4

---
+++ com/centraleNantes/poei2/ErwanExercice/Erwan_Exercice/src/Exercice4.java
@@ -0,0 +1,9 @@
+package com.centraleNantes.poei2.ErwanExercice.Erwan_Exercice.src;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Exercice4 {
+    public static void main(String[] args) {
+
+    }
+}
