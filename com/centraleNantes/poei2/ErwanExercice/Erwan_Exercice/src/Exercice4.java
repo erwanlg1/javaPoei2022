286205454f8e62ca9d4ade7863271826d168dbf2
Erwan Le Guillou <erwan.leguillou@yahoo.fr>
2023-01-12 16:06:58 +0100

    exercice4

--- com/centraleNantes/poei2/ErwanExercice/Erwan_Exercice/src/Exercice4.java
+++ com/centraleNantes/poei2/ErwanExercice/Erwan_Exercice/src/Exercice4.java
@@ -1,9 +1,41 @@
 package com.centraleNantes.poei2.ErwanExercice.Erwan_Exercice.src;
 
-import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 
 public class Exercice4 {
     public static void main(String[] args) {
 
+        String langue[][] = {{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"}, {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("FR: 1 EN  2");
+        int langueChoice = scanner.nextInt();
+
+        System.out.println("Choisir un jour");
+        ;
+        int dayChoice = scanner.nextInt();
+
+
+        if (langueChoice == 1) {
+
+            if (dayChoice < 7) {
+
+                System.out.println(langue[0][dayChoice]);
+            } else {
+                System.out.println("Le jour n'existe pas");
+            }
+
+
+        } else if (langueChoice == 2) {
+
+            if (dayChoice < 7) {
+                System.out.println(langue[1][dayChoice]);
+            } else {
+                System.out.println("Le jour n'existe pas");
+            }
+        } else {
+            System.out.println("Cette langue n'existe pas");
+        }
     }
 }
